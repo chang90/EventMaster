a73e54f9e6703499d36386e4ba1a201e55ba291c
hiby90hou <chang.liu.programmer@gmail.com>
2021-03-20 01:33:55 +1100

    create activity list in react

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
+import App from './app/layout/App';
 
 ReactDOM.render(
   <React.StrictMode>
