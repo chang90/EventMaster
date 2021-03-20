e30ca5af2904347fbebb4d44c3c8bf47c67b657b
hiby90hou <chang.liu.programmer@gmail.com>
2021-03-20 17:18:33 +1100

    apply server side error handler

--- API/ActivitysController.cs
+++ API/ActivitysController.cs
@@ -12,15 +12,15 @@ namespace API.Controllers
   public class ActivitiesController : BaseApiController
   {
     [HttpGet]
-    public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken ct)
+    public async Task<IActionResult> GetActivities()
     {
-      return await Mediator.Send(new List.Query(), ct);
+      return HandleResult(await Mediator.Send(new List.Query()));
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    public async Task<IActionResult> GetActivity(Guid id)
     {
-      return await Mediator.Send(new Details.Query{Id = id});
+      return HandleResult(await Mediator.Send(new Details.Query{Id = id}));
     }
 
     [HttpPost]
@@ -33,12 +33,12 @@ namespace API.Controllers
     public async Task<IActionResult> EditActivity(Guid id, Activity activity)
     {
       activity.Id = id;
-      return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+      return HandleResult(await Mediator.Send(new Edit.Command{Activity = activity}));
     }
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteActivity(Guid id) 
     {
-      return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+      return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
     }
   }
 }
